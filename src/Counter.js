5883b89217b9a2fcabd13c606ee39096f78de11a
Merisola <meronsolomondana@gmail.com>
2025-02-13 23:34:03 +0300

    Added Counter component

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <p>Count: 0</p> {/* Placeholder for counter value */}
+    </div>
+  );
+};
+
+export default Counter;
